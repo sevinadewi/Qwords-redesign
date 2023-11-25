3e378064e87baeabed0c2e4ec7b9daa7c080d7a8
balbalcode <iqbalmaulana027@gmail.com>
2023-11-25 12:26:23 +0700

    chore: first commit

---
+++ resources/views/components/parts/banner.blade.php
@@ -0,0 +1,44 @@
+
+<div class="" style="background: url('https://www.qwords.com/wp-content/themes/qwords/assets/images/shapes/webp/main-bg.webp')">
+	<div class="container-lg pt-5">
+		<div class="d-flex">
+			<div class="py-5 px-2">
+				<p class="fs-1 mt-3 fw-bold">
+					Cloud Hosting <br> untuk <strong class="text-primary"> Kesuksesan Websitemu!</strong>
+				</p>
+				<p class="fw-light">
+					Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3. 
+				</p>
+
+				<p class="mt-5 mb-0 fw-light">
+					Jangan Lewatkan!
+				</p>
+
+				<p class="my-0 fs-6 fw-bold">
+					Cek sekarang dan dapatkan promo hingga <span class="text-danger">50%!</span>
+				</p>
+
+				<div class="d-flex mt-0">
+					<button class="btn px-5 mx-1 my-3 btn-primary rounded-pill">Cek Domain</button>
+					<button class="btn px-5 mx-1 my-3 btn-dark rounded-pill"> 🎁 Lihat Promo</button>
+				</div>
+
+
+				<p class="fs-7 mt-5">
+					Terdaftar di PSE, ICANN dan Partner Lainnya
+				</p>
+				<div class=" mx-auto w-100">
+					<img style="max-height: 64px" class="mx-1" src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/code.webp">
+					<img style="max-height: 64px" class="mx-1" src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/icann-logo.webp">
+					<img style="max-height: 64px" class="mx-1" src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/logobsi-black.webp">
+					<img style="max-height: 64px" class="mx-1" src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/badge_whmadmin.svg">
+					<img style="max-height: 64px" class="mx-1" src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/acronis-cloud-tech.webp">
+					<img style="max-height: 64px" class="mx-1" src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/acronis-cloud-sales.webp">
+				</div>
+			</div>
+			<div class="p-5 w-75">
+				<img src="/assets/images/banner.png" class="img-fluid">
+			</div>
+		</div>
+	</div>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Qwords.com: Cloud Web Hosting Indonesia Akses Cepat Aman</title>
+    <meta name="description" content="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://www.qwords.com/wp-content/uploads/2022/12/cropped-logo-qw-light-32x32.png" sizes="32x32" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="/assets/css/custom.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700&family=Secular+One&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <style type="text/css">
+       *{ font-family: 'Plus Jakarta Sans', sans-serif}
+   </style>
+   <body style="overflow-x: hidden;">
+
+   <div>
+      @include('components/base/header')
+      @include('components/parts/banner')
+      @include('components/parts/domain-checker')
+      @include('components/parts/package')
+      @include('components/parts/contact')
+      @include('components/base/footer')
+   </div>
+
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</head>
+</html>
